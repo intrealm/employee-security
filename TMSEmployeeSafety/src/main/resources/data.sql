08977b2b851e81bd7c6954619e84d3bc67e412a2
shivani <shivani.puri@sapient.com>
2019-02-20 19:50:25 +0530

    fixed compilation error

--- TMSEmployeeSafety/src/main/resources/data.sql
+++ TMSEmployeeSafety/src/main/resources/data.sql
@@ -2,6 +2,6 @@
 
 insert into user_entity values('10001','noida sector 119','noida sector 119', '1qaAaa', 'shivani puri',8860095906, 'user','shipuri');
 
-insert into route_entity values(1,false,false,15,'DROP','UP-14-1234');
+insert into route_entity values(1,1,false,false,15,'DROP','UP-14-1234');
 
 insert into route_sequence_entity values ('shipuri',false,false,'Noida','10001',1,15,1)
