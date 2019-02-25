1adfa66fd3b8978d0a31fc0efe94362096733fdf
soupatha <soupatha@WKWIN4954138.global.publicisgroupe.net>
2019-02-25 09:41:36 +0530

    adding a test user

--- TMSEmployeeSafety/src/main/resources/data.sql
+++ TMSEmployeeSafety/src/main/resources/data.sql
@@ -1,6 +1,7 @@
 
 
 insert into user_entity values('10001','noida sector 119','noida sector 119', '1qaAaa', 'shivani puri',8860095906, 'user','shipuri');
+insert into user_entity values('10002','noida sector 119','noida sector 1190', 'password', 'sourva pathak',8585912788, 'user','souravp@test.com');
 
 insert into route_entity(id,is_started,is_completed,route_number,shift,delayed_by,vehicle_number,eta_in_minutes) values(1,true,false,15,'DROP',0,'UP-14-1234',2);
 
