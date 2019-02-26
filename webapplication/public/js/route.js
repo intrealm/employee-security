f9e50a2d0838d40b739952f935d73017a2880221
Sourav Pathak <soupatha@publicisgroupe.net>
2019-02-26 16:14:01 +0530

    conversio to express static app

---
+++ webapplication/public/js/route.js
@@ -0,0 +1,66 @@
+// Normal User
+
+$( document ).ready(function() {
+    $.get( "http://localhost:9091/displayRoute/souravp", function(data) {
+            renderAdminPage(data);
+    });
+    
+    function renderAdminPage(data){
+        var admindata = data;
+
+        $("#usrname").html(admindata.userName);
+        $("#route").html(admindata.routeNumber);
+        
+        userData(data);
+    }
+    
+    function userData(data){
+        debugger;
+        console.log(data);
+        var html= '';
+           
+            html += "<div class='col-md-12'>";
+            html += "<div class='col-md-12'>";
+            html += '<p> Route Number:<span id="usrname">'+data[0].routeNumber+'</span></p>';
+            html += '<p> Name:<span id="usrname">'+data[0].userName+'</span></p>';
+            html += "</div>";
+            html += '<table class="table table-bordered">';
+            html += '<tr>';
+            html += '<td><input type="checkbox" value=""> Select All</td>';
+            html += '<td>Drop Location</td>';
+            html += '<td>Delayed By</td>';
+            html += '<td>Boarded</td>';
+            html += '<td>Deboarded</td>';
+            html += '</tr>';
+            data.forEach(function(value,index){
+            html += '<tr>';
+            html += '<td>'+value.userName+'</td>';
+            html += '<td>'+value.dropLocation+'</td>';
+            html += '<td>'+value.delayedBy+'</td>';
+               if(value.boarded == "true"){
+                     html += '<td>';
+                     html += '<input type="checkbox" value="" checked>';
+                     html += '</td>';
+               }else{
+                     html += '<td>';
+                     html += '<input type="checkbox" value="" >';
+                     html += '</td>';
+               }
+              if(value.deboarded == "true"){
+                     html += '<td>';
+                     html += '<input type="checkbox" value="" checked>';
+                     html += '</td>';
+               }else{
+                     html += '<td>';
+                     html += '<input type="checkbox" value="" >';
+                     html += '</td>';
+               }
+            html += '</tr>';   
+           
+               
+           })
+            html += '</table>';
+            html += "</div>";
+         $("#drawtable").html(html);  
+    }
+});
---
+++ webapplication/public/js/scripts.js
@@ -0,0 +1,44 @@
+$( document ).ready(function() {  
+        var roleid;
+        openPage("../templates/signin.html", '');
+        $(document).on('click','.btn', login);
+    
+        function login(){
+            var myform = document.getElementById("signin");
+                var fd = new FormData(myform );
+                $.ajax({
+                    url: "http://localhost:9091/login",
+                    data: fd,
+                    cache: false,
+                    processData: false,
+                    contentType: false,
+                    type: 'POST',
+                    success: function (dataofconfirm) {
+                        if(dataofconfirm.roleId){
+                           localStorage.setItem("roleid", dataofconfirm.roleId);
+                           }
+                        
+                        if(dataofconfirm.roleId == "user"){
+                            openPage('templates/route.html', dataofconfirm);
+                        }else if(dataofconfirm.roleId == "admin"){
+                            openPage('templates/routelist.html', dataofconfirm);
+                        }else{
+                            alert("Unable to login")
+                        }
+                          
+                    }
+                });
+        }
+ 
+      function openPage(url, loginData){
+           $.ajax({
+                url: url,
+                type: 'GET',
+                success: function (data) {
+                    $("#pageview").html(data);
+                  
+                }
+            });
+
+       }
+});
